0ac13f0ccf382b1cf5554061641861ddb774156c
David Konsumer <konsumer@jetboystudio.com>
2021-09-29 19:22:15 -0700

    track active

--- components/Page.js
+++ components/Page.js
@@ -5,25 +5,27 @@ import IconGear from '@rsuite/icons/Gear'
 import { LinkNav, LinkDrop, NavLink } from 'components/link'
 import 'rsuite/styles/index.less'
 
-const NavHeader = () => (
-  <Navbar>
-    <Navbar.Brand href='/' componentClass={NavLink}>
-      RSUITE Site
-    </Navbar.Brand>
-    <Nav>
-      <LinkNav href='/news'>News</LinkNav>
-      <LinkNav href='/products'>Products</LinkNav>
-      <Dropdown title='About'>
-        <LinkDrop href='/company'>Company</LinkDrop>
-        <LinkDrop href='/team'>Team</LinkDrop>
-        <LinkDrop href='/contact'>Contact</LinkDrop>
-      </Dropdown>
-    </Nav>
-    <Nav pullRight>
-      <LinkNav icon={<IconGear />} href='/settings'>Settings</LinkNav>
-    </Nav>
-  </Navbar>
-)
+const NavHeader = () => {
+  return (
+    <Navbar>
+      <Navbar.Brand href='/' as={NavLink}>
+        RSUITE Site
+      </Navbar.Brand>
+      <Nav>
+        <LinkNav href='/news'>News</LinkNav>
+        <LinkNav href='/products'>Products</LinkNav>
+        <Dropdown title='About'>
+          <LinkDrop href='/company'>Company</LinkDrop>
+          <LinkDrop href='/team'>Team</LinkDrop>
+          <LinkDrop href='/contact'>Contact</LinkDrop>
+        </Dropdown>
+      </Nav>
+      <Nav pullRight>
+        <LinkNav icon={<IconGear />} href='/settings'>Settings</LinkNav>
+      </Nav>
+    </Navbar>
+  )
+}
 
 const Page = ({ title = 'RSUITE Site', children }) => (
   <div>
--- components/link.js
+++ components/link.js
@@ -2,10 +2,11 @@
 
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { Button, Nav, Dropdown, Breadcrumb } from 'rsuite'
 import 'rsuite/styles/index.less'
 
-export const NavLink = React.forwardRef((props, ref) => {
+const NavLink = React.forwardRef((props, ref) => {
   const { as, href, ...rest } = props
   return (
     <Link href={href} as={as}>
@@ -14,7 +15,14 @@ export const NavLink = React.forwardRef((props, ref) => {
   )
 })
 
-export const LinkNav = props => <Nav.Item componentClass={NavLink} {...props} />
-export const LinkDrop = props => <Dropdown.Item componentClass={NavLink} {...props} />
-export const LinkButton = props => <Button componentClass={NavLink} {...props} />
-export const LinkBread = props => <Breadcrumb.Item componentClass={NavLink} {...props} />
+export function wrap (Component) {
+  return props => {
+    const router = useRouter()
+    return <Component active={props.href === router.pathname} as={NavLink} {...props} />
+  }
+}
+
+export const LinkNav = wrap(Nav.Item)
+export const LinkDrop = wrap(Dropdown.Item)
+export const LinkButton = wrap(Button)
+export const LinkBread = wrap(Breadcrumb.Item)
